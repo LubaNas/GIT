bfdcce972ab58b939ae93b7ab4bb2bbdfaa13965
LubaNas <lnaseykina@gmail.com>
2020-02-29 12:34:34 -0500

    update from InteliJ

--- src/test/Login.java
+++ src/test/Login.java
@@ -3,5 +3,7 @@ package test;
 public class Login {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Java is fun with Murodil!");
+        // this line is written in InteliJ;git
     }
 }
